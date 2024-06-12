36ab162a855efca386bc6d335858c3c19812bef5
Kanak-Kandare <114418221+Kanak-Kandare@users.noreply.github.com>
2024-06-12 10:54:12 +0530

    base setup

---
+++ bookshelf/server/routes/routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const { registerUser, authUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', authUser);
+
+module.exports = router;
